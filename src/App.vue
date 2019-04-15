597ec42cab8f629c0558d4108b8c6f042f357370
YJKZHUZHU <yejunkuan@163.com>
2019-04-15 20:42:08 +0800

    first commmit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  console.log(window.innerWidth)
+  let fontSize = window.innerWidth / 10 + 'px'
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/Ebook.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ebook">
+    <dir class="read-wrapper">
+      <div id="read"></div>
+    </dir>
+  </div>
+</template>
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/167176.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    // 电子书的解析和渲染
+    showEpub() {
+      // 生成Book
+      this.book = new Epub(DOWNLOAD_URL)
+      // 生成Rendition
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过Rendtin.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted() {
+    this.showEpub()
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+@import 'assets/styles/gloabal.scss'
+
+</style>
+
+
